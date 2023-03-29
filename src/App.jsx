9e2f71fdddbb5ee1f7c7c8ef1c9e678502130c9e
Juan Penaloza <penalozahernandezjuandavid03@gmail.com>
2023-03-29 01:03:58 -0500

    implementación del carousel 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Carousel from "./components/Carousel";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Carousel />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Carousel.css";
+
+export default function Carousel() {
+  return (
+    <section className="carousel">
+      <img src="img/mobile-1.jpg" alt="Logo mobile 1" />
+      <img src="img/mobile-2.jpg" alt="Logo mobile 2" />
+      <img src="img/mobile-3.jpg" alt="Logo mobile 3" />
+      <img src="img/computer-1.jpg" alt="Logo computer 1" />
+      <img src="img/computer-2.jpg" alt="Logo computer 2" />
+      <img src="img/computer-3.jpg" alt="Logo computer 3" />
+    </section>
+  );
+}
